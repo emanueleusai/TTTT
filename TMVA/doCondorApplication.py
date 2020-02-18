e120e15dad60f3c2109b474ad53b62d39a34876a
Emanuele Usai <ema2487@gmail.com>
2020-02-17 22:57:43 -0500

    adapt to 2020Feb10

--- TMVA/doCondorApplication.py
+++ TMVA/doCondorApplication.py
@@ -6,19 +6,20 @@ BDT = 'BDT'
 
 varListKey = 'BigComb'
 #templateFile = '/user_data/jlee/TTTT/CMSSW_9_4_6_patch1/src/TMVA/TMVAClassificationApplication_template.C'
-templateFile = '/home/mhadley/testBDTFromJangbae/CMSSW_9_4_6_patch1/src/TTTT/TMVA/TMVAClassificationApplication_template.C'
+templateFile = '/home/eusai/4t/TTTT/TMVA/TMVAClassificationApplication_template.C'
 # massList = ['Low1','Low2']
 #weightFile = '/user_data/jlee/TTTT/CMSSW_9_4_6_patch1/src/TMVA/dataset/weights/'
-weightFile = '/home/mhadley/testBDTFromJangbae/CMSSW_9_4_6_patch1/src/TTTT/TMVA/dataset/weights/'
-weightFile+= BDT+'_BigComb_3vars_mDepth2/TMVAClassification_'+BDT+'.weights.xml'
+weightFile = '/home/eusai/4t/TTTT/TMVA/dataset2020Feb10/weights/'
+weightFile+= BDT+'_BigComb_61vars_mDepth2/TMVAClassification_'+BDT+'.weights.xml'
 
 #IO directories must be full paths
 #relbase   = '/user_data/jlee/TTTT/CMSSW_9_4_6_patch1/'
-relbase = '/home/mhadley/testBDTFromJangbae/CMSSW_9_4_6_patch1/'
+relbase = '/home/eusai/4t/CMSSW_10_2_16_UL/'
 
-inputDir  = '/isilon/hadoop/store/user/mhadley/TTTT/LJMet94X_1lepTT_022719_step2_saraBDay'
+inputDir  = '/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_02052020_step2'
 #outputDir = '/isilon/hadoop/store/user/jblee/TTTT/LJMet94X_1lepTT_022719_step2_saraBDay_'+BDT+'_BigComb_3var/'+shift+'/'
-outputDir = '/isilon/hadoop/store/user/mhadley/LJMet94X_1lepTT_022719_step2_saraBDay_'+BDT+'_BigComb_3var/'+shift+'/'
+outputDir = '/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_02182020_step3_61var/'+shift+'/'
+# outputDir = '/isilon/hadoop/store/user/mhadley/LJMet94X_1lepTT_022719_step2_saraBDay_'+BDT+'_BigComb_3var/'+shift+'/'
 
 
 inputDir += '/'+shift+'/'
@@ -62,7 +63,7 @@ os.system('mkdir -p '+outputDir)
 count=0
 for file in rootfiles:
     if '.root' not in file: continue
-    if 'QCD_HT300to500_TuneCP5' not in file: continue
+    # if 'QCD_HT300to500_TuneCP5' not in file: continue
     rawname = file[:-6]
     print file
     count+=1
@@ -86,7 +87,7 @@ Arguments = %(INPUTDIR)s %(OUTPUTDIR)s %(FILENAME)s.root %(BDT)s %(CONDORDIR)s
 Queue 1"""%dict)
     jdf.close()
     os.chdir('%s/'%(condorDir))
-    os.system('condor_submit %(FILENAME)s.job'%dict)
+    # os.system('condor_submit %(FILENAME)s.job'%dict)
     os.system('sleep 0.5')                                
     os.chdir('%s'%(runDir))
     print count, "jobs submitted!!!"
