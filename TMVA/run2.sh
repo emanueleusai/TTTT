6a210abf7bbe2b4eea3ebac5537198f2bdd2e407
Emanuele Usai <ema2487@gmail.com>
2020-11-09 02:05:23 -0500

    run all

--- TMVA/run2.sh
+++ TMVA/run2.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 #source run.sh 2017 73 4
 #source check.sh 2017 73 4
-source run.sh 2017 73 6
-sleep 120
-source run.sh 2018 73 4
-sleep 120
-source run.sh 2018 73 6
-sleep 120
+# source run.sh 2017 73 6
+# sleep 120
+# source run.sh 2018 73 4
+# sleep 120
+# source run.sh 2018 73 6
+# sleep 120
 
 
-source run.sh 2017 50 4
-sleep 120
-source run.sh 2017 50 6
-sleep 120
-source run.sh 2018 50 4
-sleep 120
-source run.sh 2018 50 6
-sleep 120
+# source run.sh 2017 50 4
+# sleep 120
+# source run.sh 2017 50 6
+# sleep 120
+# source run.sh 2018 50 4
+# sleep 120
+# source run.sh 2018 50 6
+# sleep 120
 
-source run.sh 2017 40 4
-sleep 120
-source run.sh 2017 40 6
-sleep 120
-source run.sh 2018 40 4
-sleep 120
-source run.sh 2018 40 6
+# source run.sh 2017 40 4
+# sleep 120
+# source run.sh 2017 40 6
+# sleep 120
+# source run.sh 2018 40 4
+# sleep 120
+# source run.sh 2018 40 6
 
-sleep 3600
+# sleep 3600
 
 source check.sh 2017 73 4
 source check.sh 2017 73 6
